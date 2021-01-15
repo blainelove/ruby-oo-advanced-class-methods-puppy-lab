bdb7a551520b6caa7dee5ce75f7669ba781456b2
Blaine Love <blainelove94@gmail.com>
2021-01-14 21:55:51 -0500

    commit tree 2d8f75e3f16c0692c14e16a50bfa759e5bed37e9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,42 @@
 # Add your code here
+require "pry"
+
+class Dog
+
+
+    @@all = []
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def self.name
+        name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+
+    def self.print_all
+        self.all.each {|obj|puts obj.name}
+    end
+
+    def save
+        self.class.all << self
+    end
+
+end
+
+
+
+a = Dog.new('a')
+b = Dog.new('b')
+c = Dog.new('c')
+# binding.pry
